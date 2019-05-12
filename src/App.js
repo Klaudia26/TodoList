82c91e336381c83360e5951879d552033be33bd5
Klaudia <klaudia.krzywda@gmail.com>
2019-05-12 19:19:27 +0100

    add todo List & add new Todo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import CreateNewTodo from './CreateNewTodo';
-class App extends React.Component{
+import TodoList from './TodoList';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            todos: [
+                {
+                    id: 0,
+                    text: "Holiday",
+                    complete: true,
+                },
+                {
+                    id: 1,
+                    text: "Shopping",
+                    complete: false,
+                }
+            ]
+        }
+    }
+    addNewTodo = (todo) => {
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                {
+                    id: 3,
+                    text: todo,
+                    complete: false,
+                }
+            ]
+        });
+    }
     render(){
-        
         return(
             <div>
-                <CreateNewTodo/>
+                <CreateNewTodo addNewTodo={this.addNewTodo} />
+                <TodoList todos={this.state.todos}/>
             </div>
             
         )
--- src/CreateNewTodo.js
+++ src/CreateNewTodo.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
 class CreateNewTodo extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            text: "",
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({ text: e.target.value });
+    }
+
+    handlerOnClick = () => {
+        this.props.addNewTodo(this.state.text);
+    }
+
     render(){
         return(
             <div>
-                <input type="text"/>
-                <button>Add New</button>
+                <input 
+                    value={this.state.text}
+                    type="text"
+                    onChange={this.handleChange}
+                 />
+                <button
+                    onClick={this.handlerOnClick}
+                >
+                    Add New
+                </button>
             </div>
         )
     }
